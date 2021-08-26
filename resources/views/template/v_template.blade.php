4ddc9f1eb445be03869ae0d91d542755bd42b99b
aabizardd <abizard@student.telkomuniversity.ac.id>
2021-08-26 22:59:56 +0700

    first commit

---
+++ resources/views/template/v_template.blade.php
@@ -0,0 +1,198 @@
+<!doctype html>
+<html lang="zxx">
+
+    <!-- Mirrored from preview.colorlib.com/theme/aranoz/index.html by HTTrack Website Copier/3.x [XR&CO'2014], Thu, 26 Aug 2021 09:44:08 GMT -->
+
+    <head>
+
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <title>Liuspulsa.com</title>
+        <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css"
+            integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p"
+            crossorigin="anonymous" />
+
+        <link rel="icon" href="{!! asset('assets/img/xfavicon.png.pagespeed.ic.pBryiaU2YA.png') !!}">
+
+        <link rel="stylesheet" href="{!! asset('css/style-2.css') !!}" />
+
+        <link rel="stylesheet" href="{!! asset('css/pagespeed.css') !!}">
+    </head>
+
+    <body>
+
+        @include('template.v_navbar')
+
+
+        @yield('content')
+
+
+
+
+        <footer class="footer_part">
+            <!-- <div class="container">
+        <div class="row justify-content-around">
+          <div class="col-sm-6 col-lg-2">
+            <div class="single_footer_part">
+              <h4>Top Products</h4>
+              <ul class="list-unstyled">
+                <li><a href="#">Managed Website</a></li>
+                <li><a href="#">Manage Reputation</a></li>
+                <li><a href="#">Power Tools</a></li>
+                <li><a href="#">Marketing Service</a></li>
+              </ul>
+            </div>
+          </div>
+          <div class="col-sm-6 col-lg-2">
+            <div class="single_footer_part">
+              <h4>Quick Links</h4>
+              <ul class="list-unstyled">
+                <li><a href="#">Jobs</a></li>
+                <li><a href="#">Brand Assets</a></li>
+                <li><a href="#">Investor Relations</a></li>
+                <li><a href="#">Terms of Service</a></li>
+              </ul>
+            </div>
+          </div>
+          <div class="col-sm-6 col-lg-2">
+            <div class="single_footer_part">
+              <h4>Features</h4>
+              <ul class="list-unstyled">
+                <li><a href="#">Jobs</a></li>
+                <li><a href="#">Brand Assets</a></li>
+                <li><a href="#">Investor Relations</a></li>
+                <li><a href="#">Terms of Service</a></li>
+              </ul>
+            </div>
+          </div>
+          <div class="col-sm-6 col-lg-2">
+            <div class="single_footer_part">
+              <h4>Resources</h4>
+              <ul class="list-unstyled">
+                <li><a href="#">Guides</a></li>
+                <li><a href="#">Research</a></li>
+                <li><a href="#">Experts</a></li>
+                <li><a href="#">Agencies</a></li>
+              </ul>
+            </div>
+          </div>
+          <div class="col-sm-6 col-lg-4">
+            <div class="single_footer_part">
+              <h4>Newsletter</h4>
+              <p>Heaven fruitful doesn't over lesser in days. Appear creeping
+              </p>
+              <div id="mc_embed_signup">
+                <form target="_blank"
+                  action="https://spondonit.us12.list-manage.com/subscribe/post?u=1462626880ade1ac87bd9c93a&amp;id=92a4423d01"
+                  class="subscribe_form relative mail_part">
+                  <input type="email" name="email" id="newsletter-form-email" placeholder="Email Address"
+                    class="placeholder hide-on-focus" onfocus="this.placeholder = ''"
+                    onblur="this.placeholder = ' Email Address '">
+                  <button type="submit" name="submit" id="newsletter-submit"
+                    class="email_icon newsletter-submit button-contactForm">subscribe</button>
+                  <div class="mt-10 info"></div>
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div> -->
+            <div class="copyright_part">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-lg-8">
+                            <div class="copyright_text">
+                                <P>
+                                    Copyright &copy;<script>
+                                    document.write(new Date().getFullYear());
+                                    </script> All rights reserved | This template is made with <i class="ti-heart"
+                                        aria-hidden="true"></i>
+                                    by <a href="https://colorlib.com/" target="_blank">Colorlib</a>
+                                </P>
+                            </div>
+                        </div>
+                        <div class="col-lg-4">
+                            <div class="footer_icon social_icon">
+                                <ul class="list-unstyled">
+                                    <li><a href="#" class="single_social_icon"><i class="fab fa-facebook-f"></i></a>
+                                    </li>
+                                    <li><a href="#" class="single_social_icon"><i class="fab fa-twitter"></i></a></li>
+                                    <li><a href="#" class="single_social_icon"><i class="fas fa-globe"></i></a></li>
+                                    <li><a href="#" class="single_social_icon"><i class="fab fa-behance"></i></a></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </footer>
+
+
+        <script src="{!! asset('js/jquery-1.12.1.min.js') !!}"></script>
+
+        <script src="{!! asset('js/popper.min.js%2bbootstrap.min.js.pagespeed.jc.i78k19mshX.js') !!}">
+        </script>
+        <script>
+        eval(mod_pagespeed_ZAswXqM8Ud);
+        </script>
+
+        <script>
+        eval(mod_pagespeed_EfrDYFnu4j);
+        </script>
+
+        <script src="{!! asset('js/jquery.magnific-popup.js') !!}"></script>
+
+        <script src="{!! asset('js/swiper.min.js') !!}"></script>
+
+        <script src="{!! asset('js/masonry.pkgd.js') !!}"></script>
+
+        <script
+            src="{!! asset('js/owl.carousel.min.js%2bjquery.nice-select.min.js%2bslick.min.js%2bjquery.counterup.min.js%2bwaypoints.min.js.pagespeed.jc.I5OJqcvoDe.js') !!}">
+        </script>
+        <script>
+        eval(mod_pagespeed_9Z4qIwpEkb);
+        </script>
+        <script>
+        eval(mod_pagespeed_MPlfZyvWKT);
+        </script>
+
+        <script>
+        eval(mod_pagespeed_S$S579wDQw);
+        </script>
+        <script>
+        eval(mod_pagespeed_Mc5PivJ711);
+        </script>
+        <script>
+        eval(mod_pagespeed_FHbok_8bU7);
+        </script>
+        <script
+            src="{!! asset('js/contact.js%2bjquery.ajaxchimp.min.js%2bjquery.form.js%2bjquery.validate.min.js%2bmail-script.js%2bcustom.js.pagespeed.jc.ntpiCA2YNB.js') !!}">
+        </script>
+        <script>
+        eval(mod_pagespeed_80ekGytx6J);
+        </script>
+        <script>
+        eval(mod_pagespeed_B9u9tEzFif);
+        </script>
+        <script>
+        eval(mod_pagespeed_ZfSEmZ$dYf);
+        </script>
+        <script>
+        eval(mod_pagespeed_nLwfJZTo5w);
+        </script>
+        <script>
+        eval(mod_pagespeed_OwXKBAn$my);
+        </script>
+
+        <script>
+        eval(mod_pagespeed_ZivL3dKadY);
+        </script>
+
+        <script async src="https://www.googletagmanager.com/gtag/js?id=UA-23581568-13"></script>
+        <script src="https://kit.fontawesome.com/a9b245ead0.js" crossorigin="anonymous"></script>
+
+    </body>
+
+    <!-- Mirrored from preview.colorlib.com/theme/aranoz/index.html by HTTrack Website Copier/3.x [XR&CO'2014], Thu, 26 Aug 2021 09:46:30 GMT -->
+
+</html>
---
+++ resources/views/v_home.blade.php
@@ -0,0 +1,357 @@
+@extends('template.v_template')
+
+@section('content')
+<section class="banner_part">
+    <div class="container">
+        <div class="row align-items-center">
+            <div class="col-lg-12">
+                <div class="banner_slider owl-carousel">
+
+                    <div class="single_banner_slider">
+                        <div class="row">
+                            <div class="col-lg-5 col-md-8">
+                                <div class="banner_text">
+                                    <div class="banner_text_iner">
+                                        <h1>Apa saja yang
+                                            kami jual ?</h1>
+                                        <p>Kami menjual antara lain:
+                                            <br>
+                                            1. Pulsa Semua Operator &emsp13; <a href="">Lihat</a>
+                                            <br>
+                                            2. Pulsa Listrik &emsp13; <a href="">Lihat</a>
+                                        </p>
+                                        <a href="#" class="btn_2">Beli Sekarang</a>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="banner_img d-none d-lg-block">
+                                <img src="img/pulsa-operator.png" alt="">
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="single_banner_slider">
+                        <div class="row">
+                            <div class="col-lg-5 col-md-8">
+                                <div class="banner_text">
+                                    <div class="banner_text_iner">
+                                        <h1>Cloth & Wood
+                                            Sofa</h1>
+                                        <p>Incididunt ut labore et dolore magna aliqua quis ipsum
+                                            suspendisse ultrices gravida. Risus commodo viverra</p>
+                                        <a href="#" class="btn_2">buy now</a>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="banner_img d-none d-lg-block" style="margin-top: -100px;">
+                                <img src="img/pulsa-caraousel.png" alt="">
+                            </div>
+                        </div>
+                    </div>
+
+
+                </div>
+                <div class="slider-counter"></div>
+            </div>
+        </div>
+    </div>
+</section>
+
+
+<section class="product_list section_padding">
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-lg-12">
+                <div class="section_tittle text-center">
+                    <h2>Hai Lius, Selamat Datang Di Website Kami! <span>Produk Yang Dijual</span></h2>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="product_list_slider owl-carousel">
+                    <div class="single_product_list_slider">
+                        <div class="row align-items-center justify-content-between">
+
+                            <div class="col-lg-2 col-sm-6">
+                                <a href="">
+                                    <div class="single_product_item">
+                                        <img src="img/product/xproduct_1.png.pagespeed.ic.zcUuYW5UEY.png" alt="">
+                                        <div class="single_product_text">
+                                            <h4>Quartz Belt Watch</h4>
+
+                                        </div>
+                                    </div>
+                                </a>
+                            </div>
+
+                            <div class="col-lg-2 col-sm-6">
+                                <div class="single_product_item">
+                                    <img src="img/product/xproduct_2.png.pagespeed.ic.o70ZeeStwq.png" alt="">
+                                    <div class="single_product_text">
+                                        <h4>Quartz Belt Watch</h4>
+
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="col-lg-2 col-sm-6">
+                                <div class="single_product_item">
+                                    <img src="img/product/xproduct_3.png.pagespeed.ic.c35V289i18.png" alt="">
+                                    <div class="single_product_text">
+                                        <h4>Quartz Belt Watch</h4>
+
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="col-lg-2 col-sm-6">
+                                <div class="single_product_item">
+                                    <img src="img/product/xproduct_4.png.pagespeed.ic.kIabirvgae.png" alt="">
+                                    <div class="single_product_text">
+                                        <h4>Quartz Belt Watch</h4>
+
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="col-lg-2 col-sm-6">
+                                <div class="single_product_item">
+                                    <img src="img/product/xproduct_5.png.pagespeed.ic.i9PyC8Y1TR.png" alt="">
+                                    <div class="single_product_text">
+                                        <h4>Quartz Belt Watch</h4>
+
+                                    </div>
+                                </div>
+                            </div>
+
+
+                        </div>
+                    </div>
+
+
+
+                </div>
+
+            </div>
+        </div>
+    </div>
+</section>
+
+
+
+<section class="product_list section_padding" style="margin-top: -150px;">
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-lg-12">
+                <div class="section_tittle text-center">
+                    <h2>Produk Kami <span>Berikut Produk kami</span></h2>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="product_list_slider owl-carousel">
+                    <div class="single_product_list_slider">
+                        <div class="row align-items-center justify-content-between">
+                            <div class="col-lg-3 col-sm-6">
+                                <div class="single_product_item">
+                                    <img src="img/product/xproduct_1.png.pagespeed.ic.zcUuYW5UEY.png" alt="">
+                                    <div class="single_product_text">
+                                        <h4>Quartz Belt Watch</h4>
+                                        <h3>$150.00</h3>
+                                        <a href="#" class="add_cart">+ add to cart<i class="ti-heart"></i></a>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-lg-3 col-sm-6">
+                                <div class="single_product_item">
+                                    <img src="img/product/xproduct_2.png.pagespeed.ic.o70ZeeStwq.png" alt="">
+                                    <div class="single_product_text">
+                                        <h4>Quartz Belt Watch</h4>
+                                        <h3>$150.00</h3>
+                                        <a href="#" class="add_cart">+ add to cart<i class="ti-heart"></i></a>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-lg-3 col-sm-6">
+                                <div class="single_product_item">
+                                    <img src="img/product/xproduct_3.png.pagespeed.ic.c35V289i18.png" alt="">
+                                    <div class="single_product_text">
+                                        <h4>Quartz Belt Watch</h4>
+                                        <h3>$150.00</h3>
+                                        <a href="#" class="add_cart">+ add to cart<i class="ti-heart"></i></a>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-lg-3 col-sm-6">
+                                <div class="single_product_item">
+                                    <img src="img/product/xproduct_4.png.pagespeed.ic.kIabirvgae.png" alt="">
+                                    <div class="single_product_text">
+                                        <h4>Quartz Belt Watch</h4>
+                                        <h3>$150.00</h3>
+                                        <a href="#" class="add_cart">+ add to cart<i class="ti-heart"></i></a>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-lg-3 col-sm-6">
+                                <div class="single_product_item">
+                                    <img src="img/product/xproduct_5.png.pagespeed.ic.i9PyC8Y1TR.png" alt="">
+                                    <div class="single_product_text">
+                                        <h4>Quartz Belt Watch</h4>
+                                        <h3>$150.00</h3>
+                                        <a href="#" class="add_cart">+ add to cart<i class="ti-heart"></i></a>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-lg-3 col-sm-6">
+                                <div class="single_product_item">
+                                    <img src="img/product/xproduct_6.png.pagespeed.ic.zcUuYW5UEY.png" alt="">
+                                    <div class="single_product_text">
+                                        <h4>Quartz Belt Watch</h4>
+                                        <h3>$150.00</h3>
+                                        <a href="#" class="add_cart">+ add to cart<i class="ti-heart"></i></a>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-lg-3 col-sm-6">
+                                <div class="single_product_item">
+                                    <img src="img/product/xproduct_7.png.pagespeed.ic.rnieT7kDrD.png" alt="">
+                                    <div class="single_product_text">
+                                        <h4>Quartz Belt Watch</h4>
+                                        <h3>$150.00</h3>
+                                        <a href="#" class="add_cart">+ add to cart<i class="ti-heart"></i></a>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-lg-3 col-sm-6">
+                                <div class="single_product_item">
+                                    <img src="img/product/xproduct_8.png.pagespeed.ic.c_yTDNfIir.png" alt="">
+                                    <div class="single_product_text">
+                                        <h4>Quartz Belt Watch</h4>
+                                        <h3>$150.00</h3>
+                                        <a href="#" class="add_cart">+ add to cart<i class="ti-heart"></i></a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+
+
+                </div>
+
+                <a href="#" class="btn_2">Show More</a>
+
+            </div>
+        </div>
+    </div>
+</section>
+
+
+<section class="our_offer section_padding">
+    <div class="container">
+        <div class="row align-items-center justify-content-between">
+            <div class="col-lg-6 col-md-6">
+                <div class="offer_img">
+                    <img src="img/pulsa-banner.jpg" alt="" style="border-radius: 20px;">
+                </div>
+            </div>
+            <div class="col-lg-6 col-md-6">
+                <div class="offer_text">
+                    <h2>Harga Murah Proses Cepat!</h2>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+
+<section class="product_list best_seller section_padding">
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-lg-12">
+                <div class="section_tittle text-center">
+                    <h2>Produk Paling Laku <span></span></h2>
+                </div>
+            </div>
+        </div>
+        <div class="row align-items-center justify-content-between">
+            <div class="col-lg-12">
+                <div class="best_product_slider owl-carousel">
+                    <div class="single_product_item">
+                        <img src="img/product/xproduct_1.png.pagespeed.ic.zcUuYW5UEY.png" alt="">
+                        <div class="single_product_text">
+                            <h4>Quartz Belt Watch</h4>
+                            <h3>$150.00</h3>
+                        </div>
+                    </div>
+                    <div class="single_product_item">
+                        <img src="img/product/xproduct_2.png.pagespeed.ic.o70ZeeStwq.png" alt="">
+                        <div class="single_product_text">
+                            <h4>Quartz Belt Watch</h4>
+                            <h3>$150.00</h3>
+                        </div>
+                    </div>
+                    <div class="single_product_item">
+                        <img src="img/product/xproduct_3.png.pagespeed.ic.c35V289i18.png" alt="">
+                        <div class="single_product_text">
+                            <h4>Quartz Belt Watch</h4>
+                            <h3>$150.00</h3>
+                        </div>
+                    </div>
+                    <div class="single_product_item">
+                        <img src="img/product/xproduct_4.png.pagespeed.ic.kIabirvgae.png" alt="">
+                        <div class="single_product_text">
+                            <h4>Quartz Belt Watch</h4>
+                            <h3>$150.00</h3>
+                        </div>
+                    </div>
+                    <div class="single_product_item">
+                        <img src="img/product/xproduct_5.png.pagespeed.ic.i9PyC8Y1TR.png" alt="">
+                        <div class="single_product_text">
+                            <h4>Quartz Belt Watch</h4>
+                            <h3>$150.00</h3>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+
+<section class="subscribe_area section_padding">
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-lg-7">
+                <div class="subscribe_area_text text-center">
+                    <h5>Join Member</h5>
+                    <h2>Masukkan nomor untuk menjadi member kami</h2>
+                    <div class="input-group">
+                        <input type="number" class="form-control" placeholder="masukkan nomor Hp"
+                            aria-label="Recipient's username" aria-describedby="basic-addon2">
+                        <div class="input-group-append">
+                            <a href="#" class="input-group-text btn_2" id="basic-addon2">Join sekarang</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+
+<section class="client_logo padding_top">
+    <div class="container">
+        <div class="row align-items-center">
+            <div class="col-lg-12">
+                <div class="single_client_logo">
+                    <img src="img/client_logo/xclient_logo_1.png.pagespeed.ic.k13XFWCmRf.png" alt="">
+                </div>
+
+            </div>
+        </div>
+    </div>
+</section>
+@endsection
